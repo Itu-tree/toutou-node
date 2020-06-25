6bd801a4b66ea7e02f21fd34335f0452943c1d72
toutou <toutounode@gmail.com>
2020-06-25 15:29:18 +0900

    add shell scripts

---
+++ up-dev.sh
@@ -0,0 +1,3 @@
+docker-compose -f nginx-proxy/docker-compose.yaml up -d 
+docker-compose -f portfolio/docker-compose.yaml up -d
+docker-compose -f portfolio/docker-compose.yaml exec -T php php artisan migrate:refresh --seed
