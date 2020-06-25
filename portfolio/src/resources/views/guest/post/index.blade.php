1de0ef83c6270dbd865704da1221af637d38476c
toutou <toutounode@gmail.com>
2020-06-25 15:33:08 +0900

    add post system

---
+++ portfolio/src/resources/views/guest/post/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">記事</div>
+                <div class="card-body">
+                    <div class="row">
+                        @foreach ($posts as $post)
+                        <div class="col-sm-6 col-md-3  mt-3">
+                            <div class="card img-thumbnail h-100">
+                                <img class="card-img-top img-fluid" src="/static/img/topic.png" alt="画像">
+                                <div class="card-body px-2 py-3">
+                                    <h5 class="card-title"><a href="{{ route('blog.post',['post'=>$post->id]) }}"
+                                            target="_blank" 　rel="noopener noreferrer">{{ $post->title }}</a>
+                                    </h5>
+                                    <div class="card-text">
+                                        <p><strong>距離 :</strong>
+                                            <br />
+                                            <a href="{{ route('blog.post',['post'=>$post->id]) }}" target="_blank"
+                                                　rel="noopener noreferrer">
+                                                {{$post->body}}</a>
+                                        </p>
+                                        <p><strong>いいね数 :</strong></p>
+                                        <p><strong>作成日 :</strong></p>
+                                    </div>
+                                </div><!-- /.card-body -->
+                            </div><!-- /.card -->
+                        </div>
+                        @endforeach
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    @endsection
--- portfolio/src/resources/views/home.blade.php
+++ portfolio/src/resources/views/home.blade.php
@@ -3,31 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-            <div class="card mb-3">
-                <div class="card-header">Dashboard</div>
-                <table class="table table-bordered mb-0">
-                    <tbody>
-                        <tr>
-                            <td>name</td>
-                            <td>{{$user->name}}</td>
-                        </tr>
-                        <tr>
-                            <td>email</td>
-                            <td>{{$user->email}}</td>
-                        </tr>
-                        <tr>
-                            <td>token</td>
-                            <td>{{$user->api_token}}</td>
-                        </tr>
-                        <tr>
-                            <td>tracking</td>
-                            <td> <a href="{{ route('player-transform') }}">tracking</a></td>
-                        </tr>
-                       
-                    </tbody>
-                </table>
-            </div>
-       
+        <div class="card mb-3">
+            <div class="card-header">Dashboard</div>
+            <table class="table table-bordered mb-0">
+                <tbody>
+                    <tr>
+                        <td>name</td>
+                        <td>{{$user->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>email</td>
+                        <td>{{$user->email}}</td>
+                    </tr>
+                    <tr>
+                        <td>token</td>
+                        <td>{{$user->api_token}}</td>
+                    </tr>
+                    <tr>
+                        <td>tracking</td>
+                        <td> <a href="{{ route('player-transform') }}">tracking</a></td>
+                    </tr>
+                    <tr>
+                        <td>記事管理</td>
+                        <td> <a href="{{ route('admin.post.manage') }}">tracking</a></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
     </div>
 </div>
 @endsection
