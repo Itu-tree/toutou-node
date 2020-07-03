816651a3ada8b7b699d74895fc204424bcfed2be
toutou <toutounode@gmail.com>
2020-07-04 01:30:50 +0900

    add feature of tag-management

--- blog/html/app/Http/Controllers/ArticleController.php
+++ blog/html/app/Http/Controllers/ArticleController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use App\ArticleTag;
 use App\Tag;
+use App\UseCases\Tag\UpdateArticleTags;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::inStatus(['public'])->get();
+        $articles = Article::inStatus(['public'])->latest('updated_at')->get();
         return view('guest.article.index', ['articles' => $articles]);
     }
 
@@ -27,14 +27,14 @@ class ArticleController extends Controller
 
     public function manage()
     {
-        $articles = Article::all();
+        $articles = Article::latest('updated_at')->get();
         return view('auth.article.index', ['articles' => $articles]);
     }
 
     public function create()
     {
         $article = Article::init();
-        return view('auth.article.edit', ['article' => $article]);
+        return view('auth.article.edit', ['article' => $article, 'tags' => Tag::all()]);
     }
 
     public function edit(Article $article)
@@ -42,23 +42,10 @@ class ArticleController extends Controller
         return view('auth.article.edit', ['article' => $article, 'tags' => Tag::all()]);
     }
 
-    // public function store(Request $request, article $article)
-    // {
-    //     $article = article::create($request->only('title', 'body', 'mdbody', 'state'));
-    //     return redirect(route('admin.article.manage'));
-    // }
-
-    public function update(Request $request, Article $article)
+    public function update(Request $request, Article $article, UpdateArticleTags $update_tags)
     {
         $article->fill($request->only('title', 'body', 'mdbody', 'state'))->save();
-        //tagの名前がuniqueか
-        //tagの新規作成
-        //$tag = Tag::create(['name'=>$tag_name])
-        //articletagの更新
-        //dd($request->tags);
-        // foreach ($request->tags as $tag) {
-        //     ArticleTag::create(['article_id' => $article->id, 'tag_id' => $tag->id]);
-        // }
+        $update_tags($article->id, $article->tags()->pluck('name')->toArray(), $request->tags[0] == null ? [] : $request->tags);
         return redirect(route('admin.article.manage'));
     }
 }
