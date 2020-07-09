c8c18b668c0dfb859cfd8c2f402ac9ecdd770fac
伊東佳樹 <ito@itouyoshikinoMacBook-Air.local>
2020-07-10 01:22:17 +0900

    modify

--- up-dev.sh
+++ up-dev.sh
@@ -1,4 +1,11 @@
+#docker network create itu_shared
+
 docker-compose -f nginx-proxy/docker-compose.yaml up -d
 docker-compose -f portfolio/docker-compose.yaml up -d 
 docker-compose -f blog/docker-compose.yaml up -d
+
+
+docker-compose -f blog/docker-compose.yaml exec -T nginx ash -c "npm install"
+docker-compose -f blog/docker-compose.yaml exec -T nginx ash -c "npm run dev"
+docker-compose -f blog/docker-compose.yaml exec -T php bash -c "composer install"
 docker-compose -f blog/docker-compose.yaml exec -T php php artisan migrate:refresh --seed
