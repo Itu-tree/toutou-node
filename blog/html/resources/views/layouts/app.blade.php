34ac712d6e7c4baa50dd532f4e277dfaca818d57
toutou <toutounode@gmail.com>
2020-07-10 20:08:52 +0900

    apply secure asset

--- blog/html/resources/views/layouts/app.blade.php
+++ blog/html/resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
     @if(isset($article) && (route('article.show',['article'=>$article->id])===url()->current() ||
     route('admin.article.show',['article'=>$article])===url()->current() ) )
