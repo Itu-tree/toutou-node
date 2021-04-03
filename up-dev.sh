8ca4bdf963c7ff6b334d110b956c1d4987382f9a
Itu-tree <toutounode@gmail.com>
2021-04-03 17:58:33 +0900

    add key generate

--- up-dev.sh
+++ up-dev.sh
@@ -8,4 +8,5 @@ docker-compose -f blog/docker-compose.yaml up -d
 docker-compose -f blog/docker-compose.yaml exec -T nginx ash -c "npm install"
 docker-compose -f blog/docker-compose.yaml exec -T nginx ash -c "npm run dev"
 docker-compose -f blog/docker-compose.yaml exec -T php bash -c "composer install"
+docker-compose -f blog/docker-compose.yaml exec -T php php artisan key:generate
 docker-compose -f blog/docker-compose.yaml exec -T php php artisan migrate:refresh --seed
