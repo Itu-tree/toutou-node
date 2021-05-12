c2ae59e4ddaa6afe6b4f1aea9e9a8939b7264519
toutou <toutounode@gmail.com>
2021-05-13 00:24:20 +0900

    add script of npm install

--- prod-update.sh
+++ prod-update.sh
@@ -2,7 +2,7 @@ docker-compose -f nginx-proxy/docker-compose.yaml up -d
 docker-compose -f portfolio/deploy.yaml up -d 
 docker-compose -f blog/deploy.yaml up -d
 
-docker-compose -f blog/deploy.yaml exec -T nginx ash -c "cd resources/ckeditor5-build-classic && npm run build && cd ../../"
+docker-compose -f blog/deploy.yaml exec -T nginx ash -c "cd resources/ckeditor5-build-classic && npm install && npm run build && cd ../../"
 docker-compose -f blog/deploy.yaml exec -T nginx ash -c "rm -rf node_modules"
 docker-compose -f blog/deploy.yaml exec -T nginx ash -c "npm cache clear --force"
 docker-compose -f blog/deploy.yaml exec -T nginx ash -c "npm install --production"
